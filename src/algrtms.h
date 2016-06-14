519265128a6e21682058e896a14c401fce4f6b1f
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2016-06-14 21:17:27 +0300

    Update algrtms.h

--- src/algrtms.h
+++ src/algrtms.h
@@ -1,9 +1,13 @@
 #ifndef ALGRTMS_H
 #define ALGRTMS_H
 
-#include <stdlib.h> // included for size_t
+#include <stdlib.h> /* included for size_t */
+#include <stdio.h> /* included for FILE */
 
-void quicksort (int arr[], int lo, int hi);
-size_t removeDuplicate (char *arr[], size_t len);
+typedef enum { FALSE, TRUE } boolean;
+
+void quicksort(int arr[], int lo, int hi);
+size_t removeDuplicate(char *arr[], size_t len);
+boolean freadUntilDelim(FILE* fp, char delim, char* dest);
 
 #endif /* ALGRTMS_H */
