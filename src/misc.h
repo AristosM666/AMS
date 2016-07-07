8f109debd323c5337790553d243820b785c161ba
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2016-07-07 12:15:25 +0300

    Update misc.h

--- src/misc.h
+++ src/misc.h
@@ -1,15 +1,21 @@
 #ifndef MISC_H
 #define MISC_H
 
-#include <stdlib.h> /* included for size_t */
-#include <stdio.h> /* included for FILE */
 
-typedef enum { FALSE, TRUE } boolean;
+#include <stdlib.h>  /* included for size_t */
+#include <stdio.h>   /* included for FILE   */
+#include <stdbool.h> /* included for bool   */
+
 
 void quicksort(int arr[], int lo, int hi);
 size_t removeDuplicate(char *arr[], size_t nmemb);
-boolean freadUntilDelim(FILE* fp, char delim, char* dest);
+bool csvReadNextVal(FILE* fp, char* dest);
 size_t getString(char* dest, size_t size);
-boolean isIntBetween(char* src, int min, int max);
+bool isIntBetween(char* src, int min, int max);
+void parseWhiteSpace(char* str);
+void fatal(char* errMsg);
+void clearScreen(void);
+void strToUpper(char* str);
+
 
 #endif /* MISC_H */
