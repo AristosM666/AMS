d7728853ce530e5bc68af42e4e98508ddababbd2
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2016-09-03 12:54:54 +0300

    Update misc.h

--- src/misc.h
+++ src/misc.h
@@ -1,18 +1,32 @@
 #ifndef MISC_H
 #define MISC_H
 
-#include <stdlib.h>  /* included for size_t */
-#include <stdio.h>   /* included for FILE   */
-#include <stdbool.h> /* included for bool   */
 
-void quicksort(int arr[], int lo, int hi);
-size_t removeDuplicate(char *arr[], size_t nmemb);
-bool csvReadNextVal(FILE* fp, char* dest);
-size_t getString(char* dest, size_t size);
-bool isIntBetween(char* src, int min, int max);
-void parseWhiteSpace(char* str);
-void fatal(char* errMsg);
-void clearScreen(void);
-void strToUpper(char* str);
+#include <stdio.h> 
+#include <stdbool.h>
+#include <sys/types.h>
+
+
+void quicksort (int arr[], ssize_t lo, ssize_t hi);
+
+size_t removeDuplicateStr (char *arr[], size_t nmemb);
+
+size_t removeDuplicate (int arr[], size_t nmemb);
+
+bool csvReadNextVal (FILE *fp, char *dest);
+
+size_t getString (char *dest, size_t size);
+
+bool isIntBetween (char *src, ssize_t min, ssize_t max);
+
+void parseWhiteSpace (char *src);
+
+void fatal (char *errMsg);
+
+void clearScreen (void);
+
+void strToUpper (char *str);
+
 
 #endif /* MISC_H */
+
