9efd42483006e4d997ab34899a78fc7846937797
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2017-08-26 22:04:42 +0300

    Update misc.h

--- src/misc.h
+++ src/misc.h
@@ -9,7 +9,7 @@
 
 void quicksort (int16_t arr[], intmax_t lo, intmax_t hi);
 size_t removeDuplicateStr (char *arr[], size_t nmemb);
-size_t removeDuplicate (int16_t arr[], size_t nmemb);
+size_t removeDuplicateInt (int16_t arr[], size_t nmemb);
 bool csvReadNextVal (FILE *fp, char *dest);
 size_t getString (char *dest, size_t size);
 bool isIntBetween (char *src, intmax_t min, intmax_t max);
@@ -17,5 +17,5 @@ void parseWhiteSpace (char *src);
 void fatal (char *errMsg);
 void pause (void);
 void clearScreen (void);
-void strToUpper (char *str);
+char *strToUpper (char *str);
 
