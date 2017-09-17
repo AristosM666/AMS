ccb050434512c7920c036294140c26b24d9ef4f8
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2017-09-17 18:49:17 +0300

    Update misc.h

--- src/misc.h
+++ src/misc.h
@@ -8,16 +8,16 @@
 #define SIZE_T_MAX (size_t) -1
 #endif
 
-void quicksort (int16_t arr[], intmax_t lo, intmax_t hi);
+void quickSort (int16_t arr[], intmax_t lo, intmax_t hi);
 size_t removeDuplicateStr (char *arr[], size_t nmemb);
 size_t removeDuplicateInt (int16_t arr[], size_t nmemb);
-bool csvReadNextVal (FILE *const fp, char *dest);
+bool csvReadValue (FILE *const fp, char *dest);
 size_t getString (char *dest, size_t size);
-bool isIntBetween (const char *src, intmax_t min, intmax_t max);
-void parseWhiteSpace (char *src);
+bool isInRange (const char *src, intmax_t min, intmax_t max);
+void stripWhiteSpace (char *src);
 void pause (void);
-void clearScreen (void);
+void clearConsole (void);
 char *strToUpper (char *str);
-void flush_stdin (void);
+void flushStdin (void);
 __attribute__((noreturn)) void fatal (const char *errMsg);
 
