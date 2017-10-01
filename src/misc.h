3e1ab509c595cabe1a12ccf2805cc15ef295239b
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2017-10-01 19:19:25 +0300

    Update misc.h

--- src/misc.h
+++ src/misc.h
@@ -8,16 +8,16 @@
 #define SIZE_T_MAX (size_t) -1
 #endif
 
-void quickSort (int16_t arr[], intmax_t lo, intmax_t hi);
-size_t removeDuplicateStr (char *arr[], size_t nmemb);
-size_t removeDuplicateInt (int16_t arr[], size_t nmemb);
-bool csvReadValue (FILE *const fp, char *dest);
-size_t getString (char *dest, size_t size);
-bool isInRange (const char *src, intmax_t min, intmax_t max);
-void stripWhiteSpace (char *src);
+void quicksort (int16_t arr[], intmax_t lo, intmax_t hi);
+size_t remove_dup_str (char *arr[], size_t nmemb);
+size_t remove_dup_int (int16_t arr[], size_t nmemb);
+bool csv_read_next (FILE *const fp, char *dest);
+size_t get_string (char *dest, size_t size);
+bool is_in_range (const char *src, intmax_t min, intmax_t max);
+void strip_white_space (char *src);
 void pause (void);
-void clearConsole (void);
-char *strToUpper (char *str);
-void flushStdin (void);
-__attribute__((noreturn)) void fatal (const char *errMsg);
+void clear_console (void);
+char *str_to_upper (char *str);
+void flush_stdin (void);
+__attribute__((noreturn)) void fatal (const char *emsg);
 
